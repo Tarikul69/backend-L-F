dbeff5c8ed22ddb52add0c2b6a4e338d8ce7a5cd
tarikulabir <tarikulabir931@gmail.com>
2023-01-16 23:25:47 +0600

    api-1

--- app/Http/Controllers/registrationController.php
+++ app/Http/Controllers/registrationController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
+function post()
+{
+     
+}
 
 class registrationController extends Controller
 {
     public function registration()
     {
-        $data = DB::select('select * from users');
-        return response()->json(['data'=>$data], 200);
+        $data = DB::table('users')->get();
+        $success =DB::table('users')->pluck('email');
+        return response()->json(["Success" =>$success, 'data'=>$data], 200);
     }
      
 }
